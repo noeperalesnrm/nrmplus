a299fa4c0f0125cf3183a718e9d465c8960b1947
nomad <noe.perales@nrm.com.mx>
2025-04-23 11:12:43 -0600

    https new CERT A

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.listen(PORT, () => {
 });
 */
 
-http.createServer(app).listen(8080);
-https.createServer(options, app).listen(8443);
+//http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
